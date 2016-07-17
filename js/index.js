1114de74ae3d6feaafc52d84356159d06e447ecf
YongYuH <larry830326@hotmail.com>
2016-07-18 02:24:19 +0800

    Add scroll and click nav animation

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
+var header_height = 93;
+var completed_users = 10;
+
 $(document).ready(function() {
-    var completed_users = 10;
-    //console.log(completed_users);
-    // // Modify user number
-    $('.users-num').text(completed_users.toString());
+    // animation
+    $(document).on("scroll", onScroll);
 
     // Join us link to form animation
     $('a[href*=#]:not([href=#])').click(function() {
@@ -17,15 +18,35 @@ $(document).ready(function() {
             }
         }
     });
-});
 
-// jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
     $('a.page-scroll').bind('click', function(event) {
+        $('a.page-scroll').removeClass("active");
+        $(this).addClass("active");
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
+            scrollTop: $($anchor.attr('href')).offset().top - header_height
+        }, 1100, 'easeOutCubic');
         event.preventDefault();
     });
+
+
+    // Modify user number
+    //console.log(completed_users);
+    $('.users-num').text(completed_users.toString());
 });
+
+function onScroll(event){
+    var scrollPos = $(document).scrollTop() + header_height;
+    $('.side-nav a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('side-nav a').removeClass("active");
+            currLink.addClass("active");
+        }
+        else {
+            currLink.removeClass("active");
+        }
+    });
+}
